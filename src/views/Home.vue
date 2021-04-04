e98c0ab4ef411ae629c98eeb35a63d599f8c8ace
Fabrício Souza Batalha <fabriciobatalha.dev@gmail.com>
2021-04-03 21:04:37 -0400

    fix

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,16 @@
 <template>
   <section>
     <h1> Produtos </h1>
-    <ProdutosBuscar/>
     <ProdutosLista/>
   </section>
 </template>
 
 <script>
-import ProdutosBuscar from "@/components/ProdutosBuscar.vue";
 import ProdutosLista from "@/components/ProdutosLista.vue";
 
 export default {
   name: "home",
   components: {
-    ProdutosBuscar,
     ProdutosLista
   },
   created() {
