4160f4494b82a3072f5f9572892df3a25863f2cc
Fabrício Souza Batalha <fabriciobatalha.dev@gmail.com>
2021-04-03 21:30:19 -0400

    layout produtos list

--- src/components/ProdutosLista.vue
+++ src/components/ProdutosLista.vue
@@ -1,9 +1,13 @@
 <template>
   <section class="produtos-container">
-    <div v-for="produto in produtos" :key="produto.id">
-      <!-- <img v-if="produto.fotos" :src="produto.fotos[0].src" :alt="produto.fotos[0].titulo"> -->
-      <p class="preco"> Nome: {{produto.name}} </p>
-    </div>
+    <transition mode="out-in">
+      <div v-if="produtos && produtos.length" class="produtos" key="produtos">
+        <div class="produto" v-for="(produto, index) in produtos" :key="index">
+            <img src="@/assets/produto.png" alt="Foto do Produto">
+            <h2 class="titulo">{{produto.name}}</h2>
+        </div>
+      </div>
+    </transition>
   </section>
 </template>
 
@@ -30,5 +34,57 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+.produtos-container {
+  max-width: 1000px;
+  margin: 0 auto;
+}
+
+.produtos {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 30px;
+  margin: 30px;
+}
+
+@media screen and (max-width: 500px) {
+  .produtos {
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: 10px;
+    margin: 10px;
+  }
+}
+
+.produto {
+  box-shadow: 0 4px 8px rgba(30, 60, 90, 0.1);
+  padding: 10px;
+  background: #fff;
+  border-radius: 4px;
+  transition: all 0.2s;
+}
+
+.produto:hover {
+  box-shadow: 0 6px 12px rgba(30, 60, 90, 0.2);
+  transform: scale(1.1);
+  position: relative;
+  z-index: 1;
+}
+
+.produto img {
+  border-radius: 4px;
+  margin-bottom: 20px;
+}
+
+.titulo {
+  margin-bottom: 10px;
+}
+
+.preco {
+  color: #e80;
+  font-weight: bold;
+}
+
+.sem-resultados {
+  text-align: center;
+}
 </style>
